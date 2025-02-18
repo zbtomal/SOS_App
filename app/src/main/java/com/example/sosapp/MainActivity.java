979e034b81ffb923b87b7dc79845ecf65c725b89
Zikrul Bari Tomal <14892096+zbtomal@users.noreply.github.com>
2025-02-18 20:57:15 +0600

    Initial commit

---
+++ app/src/main/java/com/example/sosapp/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.sosapp;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+
+        Button hospitalButton = findViewById(R.id.btn_hospital);
+        hospitalButton.setOnClickListener(v -> {
+            HospitalBottomSheet bottomSheet = new HospitalBottomSheet();
+            bottomSheet.show(getSupportFragmentManager(), "HospitalBottomSheet");
+        });
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
